384506c9f227ae025ce766c74b4ca8735b58168a
Francisco Palacios <francisco.palacios@tii.ae>
2022-01-21 13:45:57 +0000

    modulo lectura

--- src/markdown-repository/lectura.test.ts
+++ src/markdown-repository/lectura.test.ts
@@ -1,10 +1,12 @@
+import { leerMarkdown } from './lectura'
 
 describe('Tests de lectura', () => {
-  it('es capaz de leer un fichero markdown', () => {
+  it('es capaz de leer un fichero markdown', async () => {
     // Arrange
-    const ruta = './markdown-repository/ejemplo.md'
+    const ruta = './src/markdown-repository/ejemplo.md'
     // Act
-
+    const contenido = await leerMarkdown(ruta)
     // Assert
+    expect(contenido).toEqual('# titulo irrelevante')
   })
 })
--- src/markdown-repository/lectura.ts
+++ src/markdown-repository/lectura.ts
@@ -1,3 +1,7 @@
-export const leerMarkdown = (path: string): Promise<string> => {
+import { readFile } from 'fs/promises'
+const CODIFICACION_POR_DEFECTO = 'utf8'
 
+export const leerMarkdown = (ruta: string): Promise<string> => {
+  // TODO: Gestionar posibles errores
+  return readFile(ruta, { encoding: CODIFICACION_POR_DEFECTO })
 }
